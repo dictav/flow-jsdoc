2106c5ed48a776179b0c4a0212de5ff430d063cf
Shintaro Abe <dictav@gmail.com>
2016-09-05 11:44:14 +0900

    wip

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var doctrine = require("doctrine"); // JSDoc parsing
 var fs = require("fs");
 var util = require("util");
 
-function jsdocTagToFlowTag(tag) {
+function jsdocTagToFlowTag(tag, module) {
     // console.log(util.inspect(tag));
     return {
         loc: tag.title, //param|return
@@ -31,7 +31,7 @@ function extractJsdoc(comment) {
     }).map(jsdocTagToFlowTag);
 
     var returnTags = docAst.tags.filter(function(tag) {
-        return tag.title === "return";
+        return tag.title === "return" || tag.title === "returns" || tag.title === "enum";
     }).map(jsdocTagToFlowTag);
 
     return {
@@ -139,6 +139,9 @@ function getCommentedFunctionNode(node) {
             break;
         }
     }
+    if (funcDocs === null) {
+        return null;
+    }
 
     return {
         node: funcNode,
@@ -146,9 +149,100 @@ function getCommentedFunctionNode(node) {
     };
 }
 
-module.exports = function(src, opts) {
-    opts = opts || {};
+/**
+ * Retrieve a enum node along with parsed JSDoc comments for it.
+ * @param {Node} node The node to inspect.
+ * @return {?Object} An object, or null.
+ */
+function getCommentedEnumNode(node) {
+    if (!node.leadingComments) {
+        // JSDoc comments are always before the function, so if there is
+        // nothing here, we ain't interested.
+        return null;
+    }
 
+    /*
+     * We handle 2 representations:
+     * TODO: Add support @typedef
+     *
+     *     Type               Path to Function              Example
+     * ==========================================================================================
+     * VariableDeclaration   .declarations[0].init      var foo = { A:1, B:3 }
+     * ExpressionStatement   .declarations[0].init      obj.foo = { A:1, B:3 }
+     */
+    var nodeTypes = [
+        "ExpressionStatement", "VariableDeclaration"
+    ];
+    if (nodeTypes.indexOf(node.type) === -1) {
+        return null;
+    }
+
+    var funcDocs = null;
+    for (var i=0; i<node.leadingComments.length; i++) {
+        var c = node.leadingComments[i]
+        if (c.type === "Block" && c.value.match('@enum')) {
+            funcDocs = extractJsdoc(c.value);
+            break;
+        }
+    }
+    if (funcDocs === null) {
+        return null;
+    }
+
+    var enumNode = null;
+    var enumName = null;
+    switch (node.type) {
+        case "VariableDeclaration":
+            enumNode = node.declarations[0].init;
+            enumName = enumNode.parent.id.name;
+            break;
+        case "ExpressionStatement":
+            enumNode = node.expression.right;
+            enumName = enumNode.parent.left.object.name;
+            break;
+    }
+    if (enumNode === null) {
+        return null;
+    }
+
+    return {
+        node: enumNode,
+        jsdoc: funcDocs
+    }
+}
+
+function extractFuncName(node) {
+    if (!node) {
+        return null
+    }
+    switch (node.type) {
+        case 'Program':
+        case 'ExpressionStatement':
+            return null;
+        case 'MemberExpression':
+            return [extractFuncName(node.object), node.property.name]
+                .filter(function(a){ return a != null })
+                .join('.');
+        case 'AssignmentExpression':
+            return extractFuncName(node.left);
+        case 'Identifier':
+            return node.name;
+        case 'FunctionDeclaration':
+            return [extractFuncName(node.parent), node.id.name]
+                .filter(function(a){ return a != null })
+                .join('.');
+        case 'ObjectExpression': // enum
+            var obj = node.parent.id;
+            if (obj === undefined) {
+                obj = node.parent.left;
+            }
+            return extractFuncName(obj)
+        default:
+            return extractFuncName(node.parent)
+    }
+}
+
+function updateSource(src) {
     // Esprima has an undocumented 'attachComment' option which binds comments
     // to the nodes in the AST
     var output = falafel(src, {attachComment: true}, function (node) {
@@ -162,10 +256,10 @@ module.exports = function(src, opts) {
         funcNode.node.params.forEach(function(param) {
             for (i = 0; i < funcNode.jsdoc.params.length; i++) {
                 if (funcNode.jsdoc.params[i].name === param.name &&
-                        funcNode.jsdoc.params[i].type) {
+                    funcNode.jsdoc.params[i].type) {
                     // replace the function param name with the type annotated param
                     param.update(
-                        param.source() + ": " + funcNode.jsdoc.params[i].type
+                        param.source() + "/* : " + funcNode.jsdoc.params[i].type + "*/"
                     );
                 }
             }
@@ -176,10 +270,227 @@ module.exports = function(src, opts) {
         var returnDoc = funcNode.jsdoc.returns[0];
         if (returnDoc && returnDoc.type && funcNode.node.body) {
             funcNode.node.body.update(
-                ": " + returnDoc.type + " " + funcNode.node.body.source()
+                "/* : " + returnDoc.type + "*/ " + funcNode.node.body.source()
             );
         }
     });
 
     return output;
+}
+
+function outputDeclaration(src) {
+    var declarations = {}
+    var enums = []
+
+    falafel(src, {attachComment: true}, function (node) {
+        var n = getCommentedFunctionNode(node)
+        if (n) {
+            var node = n.node;
+            var jsdoc = n.jsdoc;
+
+            var params = node.params.map(function(param) {
+                return jsdoc.params
+                    .filter(function(jp) {
+                        return jp.name === param.name && jp.type
+                    })
+                    .map(function(jp) {
+                        return param.source() + ": " + jp.type
+                    })
+            });
+
+            // flatten
+            params = Array.prototype.concat.apply([], params);
+
+            var retType = jsdoc.returns[0] && jsdoc.returns[0].type;
+
+            var decName = extractFuncName(node) 
+            var arr = decName.split(".") 
+            var parentDec = declarations
+            var isPrototype = false
+            for (var i = 0; i < arr.length; i++) {
+                if (arr[i] === 'meta') {
+                    throw new Error("UnexpectedError")
+                }
+                if (arr[i] === 'prototype') {
+                    isPrototype = true
+                    parentDec.meta.type = 'class'
+                    continue
+                }
+
+                var d = parentDec[arr[i]]
+                if (!d) {
+                    d = parentDec[arr[i]] = {}
+                }
+                if (i === arr.length -1) {
+                    d.meta = {
+                        type: isPrototype ? "method" : "function",
+                        name: arr[i],
+                        params: params,
+                        retType: retType
+                    }
+                }
+                parentDec = d
+            }
+
+            return
+        }
+
+        n = getCommentedEnumNode(node)
+        if (n) {
+            var node = n.node;
+            var jsdoc = n.jsdoc;
+
+            var decName = extractFuncName(node) 
+            var values = node.properties.map(function(m) {
+                return m.value.value
+            })
+            var retType = jsdoc.returns[0] && jsdoc.returns[0].type;
+
+            var arr = decName.split(".") 
+            var parentDec = declarations
+            for (var i = 0; i < arr.length; i++) {
+                var d = parentDec[arr[i]]
+                if (!d) {
+                    d = parentDec[arr[i]] = {}
+                }
+                if (i === arr.length - 1) {
+                    d.meta = {
+                        type: 'enum',
+                        name: arr[i],
+                        source: node.source(),
+                        values: values,
+                        retType: retType
+                    }
+                }
+                parentDec = d
+            }
+
+            return
+        }
+    });
+
+    function hoge(dec, key, parent, depth) {
+        var output = ""
+        var indent = "    "
+        if (!dec) {
+            return output
+        }
+
+        if (depth === undefined) {
+            depth = -1
+        }
+
+        var m = dec.meta
+        delete dec.meta
+
+        if (!m && key) {
+            output += indent.repeat(depth) + "declare module " + key + " {\n"
+            m = {
+                module: key
+            }
+        }
+
+        if (m) {
+            if (parent && parent.module) {
+                m.module = parent.module
+            }
+
+            var params = (function(){
+              if (m.params) {
+                if (m.module) {
+                  return m.params.map(function(a) {
+                    return a.replace(m.module + '\.', '')
+                  })
+                } else {
+                  return m.params
+                }
+              } else {
+                return []
+              }
+            })()
+
+            var retType = ":" + (function(){
+              if (m.retType) {
+                if (m.module) {
+                  return m.retType.replace(m.module + '\.', '')
+                } else {
+                  return m.retType
+                }
+              } else {
+                return "void"
+              }
+            })()
+
+            switch (m.type) {
+
+                case 'class':
+                    output += indent.repeat(depth) + "declare class " + key + " {\n"
+                    if (m.name) {
+                        output += indent.repeat(depth + 1) + "constructor(" + params.join(", ") + ")"
+                            + retType + ";\n"
+                    }
+                    for (var key in dec) {
+                        output += hoge(dec[key], key, m, depth + 1)
+                    }
+                    output += indent.repeat(depth) + "}\n"
+                    return output;
+                case 'method':
+                    output += indent.repeat(depth) 
+                        + m.name + "(" + params.join(", ") + ")"
+                        + retType + ";\n"
+                    break;
+
+                case 'function':
+                    var decStr = "declare function "
+                    if (parent && parent.type === 'class') {
+                        decStr = "static "
+                    }
+                    output += indent.repeat(depth) + decStr
+                        + m.name + "(" + params.join(", ") + ")"
+                        + retType + ";\n"
+                    break;
+                case 'enum':
+                    enums.push(m.name)
+                    var idt = indent.repeat(depth)
+                    output += idt
+                        + "declare var " + m.name 
+                        + ":" + m.source.replace(/\n/g, "\n"+idt) + "\n"
+                    output += indent.repeat(depth)
+                        + "declare type " + m.name + "Value"
+                        + " = " + m.values.join("|")
+                        + ";\n"
+                default:
+                    break;
+            }
+        }
+
+        for (var k in dec) {
+            output += hoge(dec[k], k, m, depth + 1)
+        }
+
+        if (m && m.module && depth === 0) {
+            output += indent.repeat(depth) + "}\n"
+        }
+
+        return output
+    }
+
+    var output = hoge(declarations)
+
+    // rename enum
+    enums.forEach(function(a) {
+        var reg = new RegExp('(:\\s*\\??\\s*)' + a, 'g')
+        output = output.replace(reg, '$1' + a+'Value')
+    });
+    return output
+}
+
+module.exports = function(src, opts) {
+    opts = opts || {declaration: true};
+
+    if (opts.declaration) {
+        return outputDeclaration(src);
+    } else {
+        return updateSource(src);
+    }
 };
